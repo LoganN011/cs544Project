ffc698f94502ec74a5d28edfea5fc26e7864b3bd
lnunno1 <lnunno1@unm.edu>
2026-03-27 10:07:24 -0600

    added face and eye detection. Needs work but also need to add values to process them later

--- Test.py
+++ Test.py
@@ -1,6 +1,10 @@
-import cv2 as cv
+import cv2
 
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('resources/haarcascade_frontalface_default.xml')
+##The eyes part does not work very well and has alot of extra things that show
+eye_cascade = cv2.CascadeClassifier('resources/haarcascade_eye.xml')
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -9,10 +13,22 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
 
-    cv.imshow('Live Feed', frame)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    #Mess around with the value of this or preprocess the img more
+    eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow('Live Feed', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
