f9deacf0ec2c7134b6606d58bcd5b56617581e3f
lnunno1 <lnunno1@unm.edu>
2026-03-27 09:44:26 -0600

    added open and showing face on the camera

--- Test.py
+++ Test.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    cv.imshow('Live Feed', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
